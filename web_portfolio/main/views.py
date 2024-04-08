d7c53344f06976f73da98056ef559dd4781236a5
grister <carroll.lewi@gmail.com>
2024-04-08 18:55:19 +0300

    added base.html and main-page template

--- web_portfolio/main/views.py
+++ web_portfolio/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-class UserListAPIView(ListView):
-    pass
+class UserListAPIView(TemplateView):
+    template_name = 'main/index.html'
