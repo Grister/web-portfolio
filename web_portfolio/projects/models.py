c9715ec5d7ce44c35f3338b436d48f86d072e521
grister <carroll.lewi@gmail.com>
2024-04-11 18:03:05 +0300

    fixed images mapping and dynamically content

--- web_portfolio/projects/models.py
+++ web_portfolio/projects/models.py
@@ -5,10 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=36)
     link = models.URLField()
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='media/projects_photos')
+    image = models.ImageField(upload_to='projects_photos', default='/projects_photos/default.png')
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=36)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='skills')
-    image = models.ImageField(upload_to='media/skills_photos')
+    image = models.ImageField(upload_to='skills_photos')
--- web_portfolio/web_portfolio/urls.py
+++ web_portfolio/web_portfolio/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('projects.urls', namespace='projects')),
     path('', include('main.urls', namespace='main')),
     path('', include('contacts.urls', namespace='contacts')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
