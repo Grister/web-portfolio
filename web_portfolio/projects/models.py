59116bbafd0691ce9a6005f007e93d2b7945bf1e
grister <carroll.lewi@gmail.com>
2024-04-12 00:37:44 +0300

    added fixtures

--- web_portfolio/projects/models.py
+++ web_portfolio/projects/models.py
@@ -7,8 +7,14 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='projects_photos', default='/projects_photos/default.png')
 
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=36)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='skills')
     image = models.ImageField(upload_to='skills_photos')
+
+    def __str__(self):
+        return f'{self.project.title} - {self.name}'
