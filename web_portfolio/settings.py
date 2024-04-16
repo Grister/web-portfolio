45aa1c4fe603789acab2fd89be33e4230f808326
Захар Кравченко <45691143+Grister@users.noreply.github.com>
2024-04-16 20:06:27 +0300

    Update settings.py

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -13,6 +13,7 @@ import os
 
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'web_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
