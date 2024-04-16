c2c0bce27e1ecfda070d61eae38f982988cfd859
Захар Кравченко <45691143+Grister@users.noreply.github.com>
2024-04-16 15:22:53 +0300

    Update settings.py

--- web_portfolio/web_portfolio/settings.py
+++ web_portfolio/web_portfolio/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 load_dotenv(dotenv_path=BASE_DIR / '.env.dev')
 # Quick-start development settings - unsuitable for production
