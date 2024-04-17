dfbf4a23aee8a4538df343c1270e8fedef085dcd
Grister <carroll.lewi>
2024-04-17 14:56:09 +0300

    del commit

--- web_portfolio/contacts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from contacts import views
-
-app_name = 'contacts'
-
-urlpatterns = [
-    path("contact_me/", views.ContactsView.as_view(), name="contacts"),
-]
--- web_portfolio/contacts/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-from common.views import TitleMixin
-
-
-class ContactsView(TitleMixin, TemplateView):
-    template_name = 'contacts/contacts.html'
-    title = "Contact me"
